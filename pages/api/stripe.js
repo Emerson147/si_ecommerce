7c1a685ce11fbe3fde26f0351ec15a80edb35287
Emerson <emersontec147@gmail.com>
2023-05-19 02:08:34 -0500

    Primer commit - Pago con stripe

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -4,6 +4,15 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const params = {
+        submit_type: `pay`,
+        mode: `payment`,
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: [
+          { shipping_rate: "shr_1N8GjDGgNznnjzwjw71mvxpq" },
+          { shipping_rate: "shr_1N8GkxGgNznnjzwjjvO8NTak" },
+        ],
+
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ const Home = ({ products, bannerData }) => (
     <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
     <div className="products-heading">
-      <h2>Best Selling Products</h2>
+      <h2>Productos mas vendidos</h2>
       <p>Speakers of many variations passages</p>
     </div>
 
